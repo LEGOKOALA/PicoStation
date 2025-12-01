7618dba386e463637beb5a46812155b0fd5543ad
MI7xZY <144147004+LEGOKOALA@users.noreply.github.com>
2025-12-01 12:22:31 -0500

    almost done

---
+++ Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+public partial class PauseMenu : CanvasLayer
+{
+	private Button resumeButton;
+	private Button optionsButton;
+	private Button exitButton;
+	private VBoxContainer vbox;
+
+	public override void _Ready()
+	{
+		vbox = GetNode<VBoxContainer>("VBoxContainer");
+		vbox.MouseFilter = Control.MouseFilterEnum.Stop;
+
+		resumeButton = GetNode<Button>("VBoxContainer/ResumeButton");
+		optionsButton = GetNode<Button>("VBoxContainer/OptionsButton");
+		exitButton = GetNode<Button>("VBoxContainer/ExitButton");
+
+		resumeButton.Pressed += OnResumePressed;
+		optionsButton.Pressed += OnOptionsPressed;
+		exitButton.Pressed += OnExitPressed;
+
+		Visible = false;
+	}
+
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("Pause"))
+		{
+			TogglePause();
+		}
+	}
+
+	private void TogglePause()
+	{
+		Visible = !Visible;
+		GetTree().Paused = Visible;
+
+		if (Visible)
+			resumeButton.GrabFocus();
+	}
+
+	private void OnResumePressed()
+	{
+		TogglePause();
+	}
+
+	private void OnOptionsPressed()
+	{
+		GD.Print("Options clicked!");
+	}
+
+	private void OnExitPressed()
+	{
+		GetTree().Paused = false; // unpause before changing scene
+		GetTree().ChangeSceneToFile("res://Scenes/StartMenu.tscn");
+	}
+}
